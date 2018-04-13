43977273c8af0b6c3b0b4dc1e516c4cb923e9043
Mateo Robayo <mapedorr@gmail.com>
2018-04-13 00:38:40 -0500

    Project created
    First models imported to the project.
    Scripting started.

---
+++ PuzzleGo/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+	public Vector3 destination;
+	public bool isMoving = false;
+	public iTween.EaseType easeType;
+
+	public float moveSpeed = 1.5f;
+	public float delay = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void Move(Vector3 destinationPos, float delayTime = 0.25f) {
+		
+	}
+}
